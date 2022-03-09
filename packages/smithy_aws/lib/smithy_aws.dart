0149bbbc4f91764c74e0d883b10b45df6f4fc9b9
Dillon Nys <nydillon@amazon.com>
2022-03-09 09:48:19 -0700

    Remove AWS checksum interceptor

--- packages/smithy_aws/lib/smithy_aws.dart
+++ packages/smithy_aws/lib/smithy_aws.dart
@@ -7,7 +7,6 @@ export 'src/endpoint/aws_endpoint_resolver.dart';
 export 'src/endpoint/credential_scope.dart';
 export 'src/endpoint/partition.dart';
 
-export 'src/http/interceptors/aws_http_checksum_interceptor.dart';
 export 'src/http/interceptors/with_sig_v4.dart';
 export 'src/protocol/aws_json_1_0.dart';
 export 'src/protocol/aws_json_1_1.dart';

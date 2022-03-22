161d5a5f2da6d18101011f67076a9804a1a507f6
Dillon Nys <nydillon@amazon.com>
2022-03-22 10:11:20 -0700

    chore: Move structure/operation tests out of goldens

--- packages/goldens/lib/structure_generator/lib/src/test/operation/get_foo_operation.dart
+++
@@ -1,3 +0,0 @@
-// Generated code. DO NOT MODIFY.
-
-library structure_generator.test.operation.get_foo_operation;
--- packages/goldens/lib/structure_generator/lib/src/test/test_client.dart
+++
@@ -1,9 +0,0 @@
-// Generated code. DO NOT MODIFY.
-
-library structure_generator.test.test_client;
-
-class TestClient {
-  const TestClient({required Uri baseUri}) : _baseUri = baseUri;
-
-  final Uri _baseUri;
-}
